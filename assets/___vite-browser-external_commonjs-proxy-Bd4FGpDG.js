a4bcfda9fffc764252984b0d63f889b5d9bcfc0a
QuietStainer <quietstainer@gmail.com>
2025-01-10 04:46:49 -0500

    initial commit

---
+++ assets/___vite-browser-external_commonjs-proxy-Bd4FGpDG.js
@@ -0,0 +1,9 @@
+import { f as e } from "./index-BV35e4bv.js";
+const t = {},
+  r = Object.freeze(
+    Object.defineProperty({ __proto__: null, default: t }, Symbol.toStringTag, {
+      value: "Module",
+    })
+  ),
+  a = e(r);
+export { a as r };
